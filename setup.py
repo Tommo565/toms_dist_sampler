c99db3be0494cdfe82474cc85c0726515e74bf8f
Tom Ewing <tomewing@toms-mbp.mynet>
2018-11-19 19:46:02 +0000

    Modified setup.py

--- setup.py
+++ setup.py
@@ -7,22 +7,23 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='toms-dist-sampler',
-    version='0.1',
+    version='0.0.1',
     description=(
         'A distribution sampler for Normal, Poisson and Binomial'
-        'distributions'
+        'distributions.'
     ),
     url='https://github.com/Tommo565/distribution-sampler',
     author='Flying Circus',
-    author_email='tomewing1979@yahoo.co.uk',
+    author_email='ewingt1979@gmail.com',
     license='MIT',
-    packages=['toms_dist_sampler'],
+    packages=setuptools.find_packages(),
     zip_safe=False,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License ::  MIT License',
         'Programming Language :: Python :: 3.7',
         'Topic :: Statistics :: Data Science',
+        "Operating System :: OS Independent",
     ],
     long_description=long_description,
     long_description_content_type='text/markdown'
