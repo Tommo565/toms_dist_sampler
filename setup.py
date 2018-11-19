4c06d98865dc1d78053c1cef2b10234f3ce7b78b
Tom Ewing <tomewing@toms-mbp.mynet>
2018-11-19 19:59:58 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+import setuptools
 
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-setup(
+setuptools.setup(
     name='toms-dist-sampler',
     version='0.0.1',
     description=(
